69b9dc7ff9ba1b3c709824af3d8ec6914ddb33fb
shivishbrahma <pur.cho.99@gmail.com>
2020-08-03 02:24:34 +0530

    Added UI Changes for House API

--- tflite-server.py
+++ tflite-server.py
@@ -7,12 +7,17 @@ import sys
 import numpy as np
 import tflite_runtime.interpreter as tflite
 from fastapi import FastAPI, File, HTTPException, UploadFile
+from fastapi.responses import HTMLResponse
 from PIL import Image
 from PIL import ImageOps
 
 from helpers import classify_image, read_labels, set_input_tensor
 
-app = FastAPI()
+app = FastAPI(
+    title="Stargazers API",
+    description="An Integrated API service for document validation for Stargazers App",
+    version="1.0.1"
+)
 
 # Settings
 MIN_CONFIDENCE = 0.1  # The absolute lowest confidence for a detection.
@@ -59,19 +64,20 @@ seg_interpreter.allocate_tensors()
 seg_input_size = seg_input_details[0]['shape'][2], seg_input_details[0]['shape'][1]
 
 
-
-
 @app.get("/")
 async def info():
-    return "tflite-server docs at ip:port/docs"
+    with open("index.html", "r") as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content, status_code=200)
 
 
 @app.post(STAGE_URL)
-async def predict_stage(image: UploadFile = File(...)):
+async def classify_house_stage(image: UploadFile = File(...)):
     try:
         contents = await image.read()
         image = Image.open(io.BytesIO(contents))
-        resized_image = image.resize((stage_input_width, stage_input_height), Image.ANTIALIAS)
+        resized_image = image.resize(
+            (stage_input_width, stage_input_height), Image.ANTIALIAS)
         results = classify_image(stage_interpreter, image=resized_image)
         label_id, prob = results[0]
         data = {}
@@ -82,15 +88,15 @@ async def predict_stage(image: UploadFile = File(...)):
     except:
         e = sys.exc_info()[1]
         raise HTTPException(status_code=500, detail=str(e))
-        
 
 
 @app.post(HVAL_URL)
-async def predict_stage(image: UploadFile = File(...)):
+async def validate_house(image: UploadFile = File(...)):
     try:
         contents = await image.read()
         image = Image.open(io.BytesIO(contents))
-        resized_image = image.resize((hval_input_width, hval_input_height), Image.ANTIALIAS)
+        resized_image = image.resize(
+            (hval_input_width, hval_input_height), Image.ANTIALIAS)
         results = classify_image(hval_interpreter, image=resized_image)
         label_id, prob = results[0]
         data = {}
@@ -102,25 +108,28 @@ async def predict_stage(image: UploadFile = File(...)):
         e = sys.exc_info()[1]
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @app.post(SEGMENTATION_URL)
-async def predict_segment(image: UploadFile = File(...)):
+async def image_segmentation(image: UploadFile = File(...)):
     try:
         contents = await image.read()
         image = Image.open(io.BytesIO(contents))
-        
+
         old_size = image.size  # old_size is in (width, height) format
         desired_ratio = seg_input_size[0] / seg_input_size[1]
         old_ratio = old_size[0] / old_size[1]
-        if old_ratio < desired_ratio: # '<': cropping, '>': padding
-        	new_size = (old_size[0], int(old_size[0] / desired_ratio))
+        if old_ratio < desired_ratio:  # '<': cropping, '>': padding
+            new_size = (old_size[0], int(old_size[0] / desired_ratio))
         else:
-        	new_size = (int(old_size[1] * desired_ratio), old_size[1])
+            new_size = (int(old_size[1] * desired_ratio), old_size[1])
         # Cropping the original image to the desired aspect ratio
         delta_w = new_size[0] - old_size[0]
         delta_h = new_size[1] - old_size[1]
-        padding = (delta_w//2, delta_h//2, delta_w-(delta_w//2), delta_h-(delta_h//2))
+        padding = (delta_w//2, delta_h//2, delta_w -
+                   (delta_w//2), delta_h-(delta_h//2))
         cropped_image = ImageOps.expand(image, padding)
-        resized_image = cropped_image.convert('RGB').resize(seg_input_size, Image.BILINEAR)
+        resized_image = cropped_image.convert(
+            'RGB').resize(seg_input_size, Image.BILINEAR)
         # Convert to a NumPy array, add a batch dimension, and normalize the image.
         image_for_prediction = np.asarray(resized_image).astype(np.float32)
         image_for_prediction = np.expand_dims(image_for_prediction, 0)
@@ -129,21 +138,23 @@ async def predict_segment(image: UploadFile = File(...)):
         seg_interpreter = tflite.Interpreter(model_path=SEGMENTATION_MODEL)
         # Invoke the interpreter to run inference.
         seg_interpreter.allocate_tensors()
-        seg_interpreter.set_tensor(seg_input_details[0]['index'], image_for_prediction)
+        seg_interpreter.set_tensor(
+            seg_input_details[0]['index'], image_for_prediction)
         seg_interpreter.invoke()
         # Retrieve the raw output map.
-        raw_prediction = seg_interpreter.tensor(seg_interpreter.get_output_details()[0]['index'])()
+        raw_prediction = seg_interpreter.tensor(
+            seg_interpreter.get_output_details()[0]['index'])()
 
-        seg_map = np.argmax(raw_prediction,axis=3)
+        seg_map = np.argmax(raw_prediction, axis=3)
         # print(seg_map)
         seg_map = np.squeeze(seg_map).astype(np.int8)
-        seg_map=np.where((seg_map<0),0,seg_map)
+        seg_map = np.where((seg_map < 0), 0, seg_map)
         seg_map_list = seg_map.tolist()
         preds = {}
         preds["seg_map"] = seg_map_list
         preds["success"] = True
         return preds
     except:
-    
+
         e = sys.exc_info()[1]
         raise HTTPException(status_code=500, detail=str(e))
